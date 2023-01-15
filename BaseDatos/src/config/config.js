fcd37d5843b9bc2d04a8dc2ea2f8100282672b55
Cintia Tissera <peque.82@gmail.com>
2023-01-14 21:03:52 -0300

    Inicio de Sesión con Passport-Final

--- BaseDatos/src/config/config.js
+++ BaseDatos/src/config/config.js
@@ -1,11 +1,5 @@
 const key = {
   
-/*     facebook: {
-        id: "1244498386280126",
-        secret:"256c351a6fa6cf596a143cdec7d741b6",
-        callbackURL:"http://localhost:8080/auth/facebook/callback" 
-
-    }, */
     mongoUrl:{ 
         value: "mongodb+srv://ommi:1234@ommidistribuidora.xof6nfa.mongodb.net/ecommerce?retryWrites=true&w=majority"
     }
--- BaseDatos/src/server.js
+++ BaseDatos/src/server.js
@@ -1,3 +1,4 @@
+//importaciones
 import express from "express";
 import session from "express-session";
 import handlebars from "express-handlebars";
@@ -14,19 +15,20 @@ import { keyValue  } from "./config/config.js";
 //conectamos a la base de datos
 const mongoUrl = keyValue.mongoUrl.value
 let userLogin= ""
+let errorMessage = ""
 
 mongoose.connect(mongoUrl,{
     useNewUrlParser: true,
     useUnifiedTopology:true
 },(error)=>{
-    if(error) return console.log(`Error de conexión a la base ${error}`);
-    console.log("conexión a la base de datos exitosa")
+    if(error) return console.log(`Hubo un error de conexión a la base ${error}`);
+    console.log("conexión a la base de datos de manera exitosa")
 });
 
 //servidor express
 const app = express();
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, ()=>console.log(`Escuchando servidor en puerto: ${PORT}`));
+app.listen(PORT, ()=>console.log(`Server listening on port ${PORT}`));
 
 
 //archivos estaticos
@@ -43,17 +45,17 @@ app.set("views", __dirname+"/views");
 app.set("view engine", ".hbs");
 
 
-//interpretacion de formato json desde el cliente
-app.use(express.json()); //lectura de json desde el cuerpo de la peticion.
-app.use(express.urlencoded({extended:true})); //lectura de json desde un metodo post de formulario
+//interpretación de formato json desde el cliente
+app.use(express.json()); //lectura de json desde el cuerpo de la petición.
+app.use(express.urlencoded({extended:true})); //lectura de json desde un método post de formulario
 
-//configuracion de la sesion
+//configuración de la sesión
 app.use(session({
     //definimos el session store
     store: MongoStore.create({
         mongoUrl: mongoUrl
     }),
-    secret:"claveSecreta", //clave de encriptaciÓn de la sesion
+    secret:"claveSecreta", //clave de encriptacion de la sesion
 
     //config para guardar en la memoria del servidor
     resave:false,
@@ -62,7 +64,7 @@ app.use(session({
 
 //configurar passport
 app.use(passport.initialize()); //conectamos a passport con express.
-app.use(passport.session());//vinculacion entre passport y las sesiones de nuestros usuarios.
+app.use(passport.session());//vinculación entre passport y las sesiones de nuestros usuarios.
 
 //serializar un usuario
 passport.serializeUser((user,done)=>{
@@ -73,44 +75,37 @@ passport.serializeUser((user,done)=>{
 passport.deserializeUser((id,done)=>{
     //validar si el usuario existe en db.
     UserModel.findById(id,(err, userFound)=>{
-        return done(err, userFound)
+       return done(err, userFound)
     })
 });
 
-//crear una función para encriptar la contraseña
+//crear una funcion para encriptar la contrase;
+// estaesmiPass => ahjsgduyqwte234296124ahsd-hash
 const createHash = (password)=>{
     const hash = bcrypt.hashSync(password,bcrypt.genSaltSync(10));
     return hash;
 }
 
-const matchPassword = (password, savedPassword)=>{
-    return new Promise((resolve, reject) => {
-        console.log("llegamos a validar?")
-      bcrypt.compare(password, savedPassword, (err, matches) => {
-        if (err) reject(err)
-        else resolve(matches)
-      })
-    })
-  }
-
-
 //estrategia de registro utilizando passport local.
 passport.use("signupStrategy", new LocalStrategy(
     {
-        passReqToCallback:true,
-        usernameField: "email"
+        usernameField: "email",
+        passwordFild:'password',
+        passReqToCallback:true
     },
     (req,username,password,done)=>{
         //logica para registrar al usuario
         //verificar si el usuario exitse en db
         UserModel.findOne({username:username},(error,userFound)=>{
+
             if(error) return done(error,null,{message:"Hubo un error"});
             if(userFound) return done(null,null,{message:"El usuario ya existe"});
+
             //guardamos el usuario en la db
             const newUser={
                 name:req.body.name,
                 username:username,
-                password:createHash(password)             
+                password:createHash(password)
             };
             UserModel.create(newUser,(error,userCreated)=>{
                 if(error) return done(error, null, {message:"Hubo un error al registrar el usuario"})
@@ -120,33 +115,47 @@ passport.use("signupStrategy", new LocalStrategy(
     }
 ));
 
-//estrategia de Login utilizando passport local.
+passport.use("Local", new LocalStrategy(
+    {
+        passReqToCallback:true,
+        usernameField: "email",
+        passwordFild:'password',
+    },
+    (req,username,password,done)=>{
+        errorMessage = ""
+        //logica para registrar al usuario
+        //verificar si el usuario exitse en db
+        UserModel.findOne({username:username},(error,userFound)=>{
+        if(error) {
+            errorMessage = "Hubo un error"
+            return done(error,null,{message:"Hubo un error"});
+        }
+            
+        if(!userFound) 
+    	{
+            errorMessage = "El usuario no existe"
+            return done(null,null,{message:"El usuario no existe"});
+        }        
+            
+        bcrypt.compare(req.body.password, userFound.password, (err, matches) => {
+            if (!matches || err) {
+                errorMessage = "Hubo un error al loguear el usuario"
+                return done(error, null, {message:"Hubo un error al loguear el usuario"})
+            }
+            else {
+                userLogin = userFound.name;
+                return done(null,userFound);
+            }
+        })
+      })
+    }
+));
 
-passport.use("local",new LocalStrategy({
-    usernameFild:'email',
-    passwordFild:'password',
-    passReqToCallback:true
-    // this working
-  },async (req,username,password,done)=> {
-    console.log("nose si llegamos aca")
-    const userFind = await UserModel.findOne({username:username})
-    if (userFind) {
-        console.log("menos si llegmos aca")
-      const validPassword = await matchPassword(toString(password),toString(userFind[0].password))
-      console.log("validPassword==>", validPassword)
-    }})
-);
 
 //rutas asociadas a las paginas del sitio web
-/* app.get("/",(req,res)=>{
-    res.render("home")
-}); */
-
-//rutas asociadas a las páginas del sitio web
 app.get("/",(req,res)=>{
     const result = (userLogin === undefined)  ? res.render("home",{users : ""}) : res.render("home",{users : userLogin })
 });
-
 
 app.get("/registro",(req,res)=>{
     const errorMessage = req.session.messages ? req.session.messages[0] : '';
@@ -155,54 +164,42 @@ app.get("/registro",(req,res)=>{
 });
 
 app.get("/inicio-sesion",(req,res)=>{
-    const errorMessage = req.session.messages ? req.session.messages[0] : '';
-    res.render("login", {error:errorMessage});
-    req.session.messages = [];
-    req.session.user = req.body.name
+    res.render("login", {error:errorMessage})
+    //const result = (errorMessage === "")  ? res.render("login",{error : ""}) : res.render("login",{error : errorMessage })
+    errorMessage=""
 });
 
 app.get("/perfil",(req,res)=>{
+    console.log(req.session)
     if(req.isAuthenticated()){
         res.render("profile");
     } else{
-        res.send("<div>Debes <a href='/inicio-sesion'>Iniciar Sesión</a> o <a href='/registro'>Registrarte</a></div>")
+        res.send("<div>Debes <a href='/inicio-sesión'>inciar sesión</a> o <a href='/registro'>Registrarte</a></div>")
     }
 });
 
-//rutas de autenticacion registro
+//ruta de autenticación registro
 app.post("/signup",passport.authenticate("signupStrategy",{
     failureRedirect:"/registro",
-    failureMessage: true
+    failureMessage: true 
 }),(req,res)=>{
     res.redirect("/perfil")
 });
 
-//ruta de autenticacion login
-
-  app.post("/login", passport.authenticate("local", {
-    //si el usuario y pass es correcto redirige a /, si no de regreso a login
-    failureRedirect: "/inicio-sesion",
-    failureMessage: true
-   }), (req, res)=> {
-    //vacio por que no se necesita
-     const result = (userLogin === undefined)  ? res.render("home",{users : ""}) : res.render("home",{users : userLogin })
-
-    res.redirect('/');
-   });
-
-
-//ruta de logout con passport
-/* app.get("/logout",(req,res)=>{
-    req.logout(err=>{
-        console.log("sdsdsds salida")
-        if(err) return res.send("Error al cerrar sesión")
-        req.session.destroy();
+//ruta de autenticación login
+    app.post("/login", passport.authenticate('Local',{ 
+        failureRedirect: '/inicio-sesion', 
+        failureMessage:true,        
+    }),(req, res)=>{
+        const result = (userLogin === undefined)  ? res.render("home",{users : ""}) : res.render("home",{users : userLogin })
         res.redirect("/")
     });
-}); */
 
+//ruta de logout con passport
 
 app.get("/logout",(req,res)=>{
     const result = (userLogin === undefined)  ? res.render("logout",{users : ""}) : res.render("logout",{users : userLogin })
+    userLogin="";
+    errorMessage="";
     req.session.destroy();
 }); 
